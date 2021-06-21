a7b791e729fed397e470229e4b77ae4c3cb5c9de
MitchellWeg <mitchell.w@live.nl>
2021-06-21 15:41:02 +0200

    add py script to download the data

---
+++ download_data.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+import pathlib
+import time
+import urllib
+import zipfile
+import requests
+
+def main():
+    pwd = os.getcwd()
+
+    dir = os.path.join(pwd + "/data")
+
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+    download_data(dir)
+    shutil.rmtree(dir)
+    os.mkdir(dir)
+    builtin_dir = os.path.join(pwd, '<built-in function dir>')
+
+    rename_files(builtin_dir, dir)
+
+def rename_files(src, dest):
+    for dir in os.listdir(src):
+        for file in os.listdir(f"{src}/{dir}"):
+            f = file.replace(' ', '_').lower()
+            os.rename(f"{src}/{dir}/{file}", f"{src}/{dir}/{f}")
+    shutil.move(src, f"{dest}/{dir}")
+
+
+def download_data(dir):
+    HOME = "https://zenodo.org/record/1167595/files/"
+    links = ['%5BC1%5D%20Ports%20of%20Brittany.zip', '%5BC1%5D%20SeaDataNet%20Port%20Index.zip', "%5BC2%5D%20European%20Coastline.zip",
+        '%5BC1%5D%20World%20Port%20Index.zip', '%5BC2%5D%20European%20Maritime%20Boundaries.zip',
+        '%5BC2%5D%20IHO%20World%20Seas.zip', '%5BC2%5D%20World%20EEZ.zip', '%5BC4%5D%20FAO%20Maritime%20Areas.zip',
+        '%5BC4%5D%20Fishing%20Areas%20%28European%20commission%29.zip', '%5BC5%5D%20Fishing%20Constraints.zip',
+        '%5BC5%5D%20Marine%20Protected%20Areas%20%28EEA%20Natura%202000%29.zip', '%5BC6%5D%20ANFR%20Vessel%20List.zip',
+        '%5BC6%5D%20EU%20Fishing%20Vessels.zip', '%5BE1%5D%20Ocean%20Conditions.zip', '%5BE2%5D%20Weather%20Conditions.zip',
+        '%5BP1%5D%20AIS%20Data.zip', '%5BP1%5D%20AIS%20Status%2C%20Codes%20and%20Types.zip', '%5BP1%5D%20Brest%20Receiver.zip',
+        '%5BQ1%5D%20Integration%20Queries.zip'
+    ]
+
+    for link in links:
+        url = f"{HOME}{link}?download=1"
+        print(f'downloading: {url}')
+
+        for i in range(0, 5):
+            try:
+                r = requests.get(url, allow_redirects=True)
+                break
+            except:
+                # if a conn error occurs,
+                # sleep for 20 seconds then retry.
+                # max retries is 5 times.
+                print("Downloading threw connection error, retrying in 20 seconds...")
+                time.sleep(20)
+                continue
+
+        filename = urllib.parse.unquote(link)
+        _zipLoc = f"{dir}/{filename}"
+        _zip = open(_zipLoc, 'wb')
+        _zip.write(r.content)
+        unzip_and_rename(_zipLoc, dir, filename)
+        _zip.close()
+        os.remove(_zipLoc)
+
+def unzip_and_rename(z, d, f):
+    f = f.split('.')[0].strip()
+    with zipfile.ZipFile(z, 'r') as zip_ref:
+        os.mkdir(f"{d}/{f}")
+        zip_ref.extractall(f"{dir}/{f}")
+
+
+if __name__ == '__main__':
+    main()
