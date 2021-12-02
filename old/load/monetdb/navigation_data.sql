0c0a66b3ebcb9cd04ca268ea9284d4f2478facfb
Ying (Jennie) Zhang <ying.zhang@monetdbsolutions.com>
2021-12-02 18:08:39 +0100

    Fixed the problem with loading the `virtual` column: we load the `f`s and `t`s first into a temp column of `char(1)` then convert them into FALSE or TRUE.

--- old/load/monetdb/navigation_data.sql
+++ old/load/monetdb/navigation_data.sql
@@ -42,15 +42,17 @@ CREATE TABLE ais_data.dynamic_aton(
   typeofaid smallint,
   aidsname text,
   virtual boolean,
+  vrt_tmp char(1),
   lon double precision,
   lat double precision,
   ts bigint,
   CONSTRAINT dynamic_aton_pkey PRIMARY KEY (id) 
 );
 
-COPY OFFSET 2 INTO ais_data.dynamic_aton (mmsi,typeofaid,aidsname,virtual,lon,lat,ts) 
-FROM '/path/to/data/[P1] AIS Data/nari_dynamic_aton.csv' (mmsi,typeofaid,aidsname,virtual,lon,lat,t) 
+COPY OFFSET 2 INTO ais_data.dynamic_aton (mmsi,typeofaid,aidsname,vrt_tmp,lon,lat,ts) 
+FROM '/path/to/data/[P1] AIS Data/nari_dynamic_aton.csv' (mmsi,typeofaid,aidsname,vrt_tmp,lon,lat,t) 
 DELIMITERS ',';
+UPDATE ais_data.dynamic_aton SET virtual = (SELECT CASE WHEN vrt_tmp = 'f' THEN FALSE ELSE TRUE END);
 
 #GEOM COLUMN
 ALTER TABLE ais_data.dynamic_aton ADD COLUMN geom Geometry;
