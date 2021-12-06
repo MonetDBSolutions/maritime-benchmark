a5b0877aaf9639672478fd03657cd1fc400fd450
Ying Zhang <ying.zhang@monetdbsolutions.com>
2021-12-06 18:21:20 +0100

    Update geographic_data.sql
    - Load the SHP files into their own schema "geographic_data"
    - No need to DROP TABLEs, if we use DROP SCHEMA ... CASCADE;

--- old/load/monetdb/geographic_data.sql
+++ old/load/monetdb/geographic_data.sql
@@ -1,37 +1,27 @@
 #
 # === Geographic Data ===
 #
-DROP TABLE IF EXISTS brittany_ports;
---call shpload('/path/to/data/[C1] Ports of Brittany/brittany_ports.shp','sys','brittany_ports');
-call shpload('/path/to/data/[C1] Ports of Brittany/port.shp','sys','brittany_ports');
+DROP SCHEMA IF EXISTS geographic_data CASCADE;
+CREATE SCHEMA geographic_data;
 
-DROP TABLE IF EXISTS seadata_ports;
-call shpload('/path/to/data/[C1] SeaDataNet Port Index/seadatanet_fishing_ports.shp','sys','seadata_ports');
+call shpload('/path/to/data/[C1] Ports of Brittany/port.shp','geographic_data','brittany_ports');
 
-DROP TABLE IF EXISTS europe_coastline;
-call shpload('/path/to/data/[C2] European Coastline/europe_coastline.shp','sys','europe_coastline');
+call shpload('/path/to/data/[C1] SeaDataNet Port Index/fishing_ports.shp','geographic_data','seadata_ports');
 
-DROP TABLE IF EXISTS fao_areas;
-call shpload('/path/to/data/[C4] FAO Maritime Areas/fao_areas.shp','sys','fao_areas');
+call shpload('/path/to/data/[C2] European Coastline/europe_coastline.shp','geographic_data','europe_coastline');
 
-DROP TABLE IF EXISTS fishing_areas;
-call shpload('/path/to/data/[C4] Fishing Areas (European commission)/v_recode_fish_area_clean.shp','sys','fishing_areas');
+call shpload('/path/to/data/[C4] FAO Maritime Areas/fao_areas.shp','geographic_data','fao_areas');
 
-DROP TABLE IF EXISTS fishing_interdiction;
-call shpload('/path/to/data/[C5] Fishing Constraints/fishing_interdiction.shp','sys','fishing_interdiction');
+call shpload('/path/to/data/[C4] Fishing Areas (European commission)/v_recode_fish_area_clean.shp','geographic_data','fishing_areas');
 
-DROP TABLE IF EXISTS brest_receptor;
-call shpload('/path/to/data/[P1] Brest Receiver/receptor.shp','sys','brest_receptor');
+call shpload('/path/to/data/[C5] Fishing Constraints/fishing_interdiction.shp','geographic_data','fishing_interdiction');
 
-DROP TABLE IF EXISTS brest_coverage;
-call shpload('/path/to/data/[P1] Brest Receiver/theoretical_coverage.shp','sys','brest_coverage');
+call shpload('/path/to/data/[P1] Brest Receiver/receptor.shp','geographic_data','brest_receptor');
 
-DROP TABLE IF EXISTS europe_borders;
-call shpload('/path/to/data/[C2] European Maritime Boundaries/mbeulsiv1.shp','sys','europe_borders');
+call shpload('/path/to/data/[P1] Brest Receiver/theoretical_coverage.shp','geographic_data','brest_coverage');
 
-DROP TABLE IF EXISTS natura_protected_areas;
-call shpload('/path/to/data/[C5] Marine Protected Areas (EEA Natura 2000)/spatial_data/Natura2000_end2015.shp','sys','natura_protected_areas');
+call shpload('/path/to/data/[C2] European Maritime Boundaries/mbeulsiv1.shp','geographic_data','europe_borders');
 
-DROP TABLE IF EXISTS wpi_ports;
-call shpload('/path/to/data/[C1] World Port Index/wpi.shp','sys','wpi_ports');
+call shpload('/path/to/data/[C5] Marine Protected Areas (EEA Natura 2000)/spatial_data/Natura2000_end2015.shp','geographic_data','natura_protected_areas');
 
+call shpload('/path/to/data/[C1] World Port Index/wpi.shp','geographic_data','wpi_ports');
