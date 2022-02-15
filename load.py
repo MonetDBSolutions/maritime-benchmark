7993533d11913f7d9dc543c5318c67ae4830b5e5
smavros <stemavros@gmail.com>
2022-02-15 14:17:15 +0100

    Formatting

--- load.py
+++ load.py
@@ -32,7 +32,8 @@ parser.add_argument('--scale', type=float, default=None, required=False,
                     help='Load the data after scaling them in scale')
 args = parser.parse_args()
 
-GENERIC_PATH='/path/to/data'
+GENERIC_PATH = '/path/to/data'
+
 
 def main():
     data_dir = os.path.abspath(args.data)
@@ -43,11 +44,11 @@ def main():
     if args.system is None or args.system == MONET_ONLY:
         # get a tuple with all monet files
         monet_scripts_root = scripts_dir + '/monetdb'
-        monet_scripts = tuple(fsTree(monet_scripts_root, '.sql')) 
+        monet_scripts = tuple(fsTree(monet_scripts_root, '.sql'))
 
         if args.scale:
             append_scale(monet_scripts, args.scale)
-        set_pwd(monet_scripts, data_dir) 
+        set_pwd(monet_scripts, data_dir)
 
         try:
             load_monetdb(scripts_dir)
@@ -55,13 +56,13 @@ def main():
             print(e)
         finally:
             set_generic_pwd(monet_scripts)
-            remove_scale(monet_scripts) 
+            remove_scale(monet_scripts)
 
     if args.system is None or args.system == PGRES_ONLY:
         # get a tuple with all postgres files
-        postgres_scripts_root = scripts_dir + '/postgres'      
+        postgres_scripts_root = scripts_dir + '/postgres'
         postgres_scripts = tuple(fsTree(postgres_scripts_root, '.sql'))
-        # append the load script for the shapefiles 
+        # append the load script for the shapefiles
         postgres_scripts += tuple([f"{scripts_dir}/load_psql.sh"])
 
         if args.bench_only:
@@ -79,8 +80,6 @@ def main():
             pass
             set_generic_pwd(postgres_scripts)
 
-# Change all instances of /path/to/data
-# with the relevant path.
 
 def fsTree(path, ext):
     entries = os.scandir(path)
@@ -90,24 +89,27 @@ def fsTree(path, ext):
         elif os.path.splitext(e.name)[1] == ext:
             yield e.path
 
+
 def append_scale(filename, sf):
     scale_tag = f'_SF_{str(sf)}'
     rgx = re.compile(r'\.csv')
     with fileinput.input(filename, inplace=True) as f:
         for line in f:
-            m = rgx.search(line) 
+            m = rgx.search(line)
             if m:
                 line = line[:m.start()] + scale_tag + line[m.start():]
             print(line, end='')
+
 
 def remove_scale(filename):
     rgx = re.compile(r'_SF_[0-9\.]*csv')
     with fileinput.input(filename, inplace=True) as f:
         for line in f:
-            m = rgx.search(line) 
+            m = rgx.search(line)
             if m:
                 line = line[:m.start()] + ".csv" + line[m.end():]
             print(line, end='')
+
 
 def set_pwd(filename, data_dir):
     with fileinput.input(filename, inplace=True) as f:
@@ -116,8 +118,9 @@ def set_pwd(filename, data_dir):
                 line = line.replace(GENERIC_PATH, data_dir)
             print(line, end='')
 
+
 def set_generic_pwd(filename):
-    # matches any '/User/blah/blah/blah/[' string 
+    # matches any '/User/blah/blah/blah/[' string
     rgx = '\/.*\['
     with fileinput.input(filename, inplace=True) as f:
         for line in f:
@@ -125,6 +128,7 @@ def set_generic_pwd(filename):
             if m:
                 line = line.replace(m.group(0)[:-2], GENERIC_PATH)
             print(line, end='')
+
 
 def open_and_split_sql_file(filename):
     try:
@@ -134,14 +138,16 @@ def open_and_split_sql_file(filename):
         return None
     return f.read().split(";\n")[:-1]
 
+
 def execute_query(cur, q):
     try:
-        #print(q)
+        # print(q)
         cur.execute(q)
     except Exception as msg:
         print(msg)
         return 0
     return 1
+
 
 def load_monetdb(scripts_dir):
     conn = pymonetdb.connect(args.database, autocommit=True)
@@ -177,42 +183,44 @@ def load_monetdb(scripts_dir):
         # for every schema
         for sd in schemas_desc:
             queries = []
-            
-            # first clear schema 
+
+            # first clear schema
             queries.append(f"DROP SCHEMA IF EXISTS {sd['schema']} CASCADE;")
             queries.append(f"CREATE SCHEMA IF NOT EXISTS {sd['schema']};")
-            
-            # load all the data through the sql scripts 
+
+            # load all the data through the sql scripts
             for s in sd['scripts']:
-                
+
                 # excluding {'bench': 'false'} ones when --benchmark-set-only
                 if args.bench_only and not s['bench']:
                     print(f"File {s['file']} is not required for the benchmark. Skiping!")
                     continue
-                
+
                 # read and split the file to individual queries
-                queries += open_and_split_sql_file(f"{scripts_dir}/monetdb/{s['file']}")
+                queries += open_and_split_sql_file(
+                    f"{scripts_dir}/monetdb/{s['file']}")
                 print(f"Loading file {s['file']}")
                 if queries is None:
                     print(f"Could not read queries in {scripts_dir}/monetdb/")
                     return 0
-                
+
                 for q in queries:
-                    execute_query(cur,q)
+                    execute_query(cur, q)
     else:
         print(f"Could not connect to MonetDB database {args.database}")
         return 0
     return 1
 
+
 def load_postgres(scripts_dir):
     conn = psycopg2.connect(f"dbname={args.database}")
     if conn:
         print(f"Connected to Postgres database {args.database}")
-        #CSV loading
+        # CSV loading
         conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
         cur = conn.cursor()
-        #Turn on postgis
-        execute_query(cur,"CREATE EXTENSION postgis;")
+        # Turn on postgis
+        execute_query(cur, "CREATE EXTENSION postgis;")
         load_files = ["navigation_data.sql",
                       "vessel_data.sql",
                       "environmental_data.sql"]
@@ -223,9 +231,9 @@ def load_postgres(scripts_dir):
                 print(f"Could not read queries in {scripts_dir}/postgres/")
                 return 0
             for q in queries:
-                if not execute_query(cur,q):
+                if not execute_query(cur, q):
                     return 0
-        #Shapefile loading
+        # Shapefile loading
         print(f"Loading Shapefiles")
         try:
             subprocess.run(
