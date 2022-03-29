98589e5fff828136dc19a13f3639d8b6a2a8c009
smavros <stemavros@gmail.com>
2022-03-29 13:04:05 +0200

    Avoids re-opening and re-reading the source file

--- scale.py
+++ scale.py
@@ -23,39 +23,47 @@ bench_files = [
 '[P1] AIS Data/nari_dynamic.csv',
 ]
 
-def scaleCSV(src, dst, scale):
-    with open(src, 'r') as input_file, \
-         open(dst, 'w+') as output_file:
+def scaleCSV(src_lines, dst, scale):
+    with open(dst, 'w+') as output_file:
         try:
-            lines = input_file.readlines()
-            new_lines_cnt = int(len(lines) * scale)
-            output_file.writelines(lines[0:new_lines_cnt])
-            logger.debug(f'Scaled {src} from {len(lines)} to {new_lines_cnt} lines')
+            new_lines_cnt = int(len(src_lines) * scale)
+            output_file.writelines(src_lines[0:new_lines_cnt])
+            logger.info(f'Scales {len(src_lines)} to {new_lines_cnt} lines')
         except IOError as msg:
             logger.warning('Scaling failed for file:{src} SF:{scale}')
             logger.exception(msg)
-    return
+
 
 def main():
-    for sf in args.scale:
-        for file in bench_files:
-             
-            scaled_file = f'{args.target}/{file[:-4]}_SF_{sf}.csv'
+
+    # for every file to be scaled
+    for file in bench_files:
+        
+        # read the lines of the input once 
+        src_lines = None
+        with open(f'{args.data}/{file}', 'r') as src_file:
+            src_lines = src_file.readlines()
+        if len(src_lines) == 0:
+            raise IOError('Source file {file} seems to be empty!')
+        logger.info(f'Going to scale {file}')
+
+        # for every scale factor
+        for sf in args.scale:
+            scaled_filename = f'{args.target}/{file[:-4]}_SF_{sf}.csv'
             
             # check if the scaled file exists 
-            if os.path.isfile(scaled_file):
-                logger.info(f'Scaled file {scaled_file} already exists!')
+            if os.path.isfile(scaled_filename):
+                logger.info(f'Scaled file {scaled_filename} already exists!')
                 continue
             else:
-                logger.info(f'Creating {scaled_file}')
-                # check first if the dir of the scaled file exists
-                directory = os.path.dirname(scaled_file)
+                # check if the dir of the scaled file exists
+                directory = os.path.dirname(scaled_filename)
                 if not os.path.exists(directory):
                     logger.info(f'Creating directory {directory}')
                     os.makedirs(directory)
-           
+                
             # scale the file
-            scaleCSV(f'{args.data}/{file}', scaled_file, sf)
+            scaleCSV(src_lines, scaled_filename, sf)
 
 
 if __name__ == "__main__":
