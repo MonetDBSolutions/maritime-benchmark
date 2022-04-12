d5acaf80cbdbf4c95080743da57eb19f2698a034
smavros <stemavros@gmail.com>
2022-04-12 13:28:54 +0200

    Extends scale script functionality for non-scaled files

--- scale.py
+++ scale.py
@@ -3,6 +3,7 @@
 import argparse
 import logging
 import os
+import shutil
 
 intro = """
         This script scales the Zenodo maritime AIS data according to the
@@ -26,7 +27,12 @@ logger = logging.getLogger(__name__)
 
 # for the time being we only scale dynamic AIS data
 bench_files = [
-'[P1] AIS Data/nari_dynamic.csv',
+    {'file': '[P1] AIS Data/nari_dynamic.csv', 'scale': True },
+    {'file': '[C1] Ports of Brittany/port.shp', 'scale': False },
+    {'file': '[C4] FAO Maritime Areas/FAO_AREAS.shp', 'scale': False },
+    {'file': '[C4] Fishing Areas (European commission)/v_recode_fish_area_clean.shp', 
+     'scale': False },
+    {'file': '[C1] World Port Index/WPI.shp', 'scale': False }
 ]
 
 def scaleCSV(src_lines, dst, scale):
@@ -39,37 +45,50 @@ def scaleCSV(src_lines, dst, scale):
             logger.warning('Scaling failed for file:{src} SF:{scale}')
             logger.exception(msg)
 
+def scaledFileExists(pathfile):
+    if os.path.isfile(pathfile):
+        logger.info(f'Scaled file {pathfile} already exists!')
+        return True
+    return False
+        
+def makeDirOfFileIfNotExists(pathfile):
+    directory = os.path.dirname(pathfile)
+    if not os.path.exists(directory):
+        logger.info(f'Creating directory {directory}')
+        os.makedirs(directory)
 
 def main():
-
     # for every file to be scaled
     for file in bench_files:
-        
-        # read the lines of the input once 
+       
         src_lines = None
-        with open(f'{args.data}/{file}', 'r') as src_file:
-            src_lines = src_file.readlines()
-        if len(src_lines) == 0:
-            raise IOError('Source file {file} seems to be empty!')
-        logger.info(f'Going to scale {file}')
+        # if the file is to be scaled read it ONCE 
+        if file['scale']:
+            # read the lines of the input once 
+            with open(f"{args.data}/{file['file']}", 'r') as src_file:
+                src_lines = src_file.readlines()
+            if len(src_lines) == 0:
+                raise IOError("Source file {file['file']} seems to be empty!")
+            logger.info(f"Going to scale {file['file']}")
 
-        # for every scale factor
+        # for every scale factor that we want to target no matter if we
+        # really scale the file or not
         for sf in args.scale:
-            scaled_filename = f'{args.target}/{file[:-4]}_SF_{sf}.csv'
             
-            # check if the scaled file exists 
-            if os.path.isfile(scaled_filename):
-                logger.info(f'Scaled file {scaled_filename} already exists!')
+            sf_tag = f'SF_{sf}' if file['scale'] else 'NOT_SCALED'
+            scaled_filename = f"{args.target}/{file['file'][:-4]}_{sf_tag}.csv"
+           
+            # if the target file exists do nothing and continue
+            if scaledFileExists(scaled_filename):
                 continue
+            # if we create the target file check if the base dir exists
+            makeDirOfFileIfNotExists(scaled_filename)
+      
+            if file['scale']:
+                scaleCSV(src_lines, scaled_filename, sf)
             else:
-                # check if the dir of the scaled file exists
-                directory = os.path.dirname(scaled_filename)
-                if not os.path.exists(directory):
-                    logger.info(f'Creating directory {directory}')
-                    os.makedirs(directory)
-                
-            # scale the file
-            scaleCSV(src_lines, scaled_filename, sf)
+                # copy to the target unscaled
+                shutil.copy2(f"{args.data}/{file['file']}", scaled_filename)
 
 
 if __name__ == "__main__":
